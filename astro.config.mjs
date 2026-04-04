e5a9f6b85e4dd68dc84eba33a33be5658ef7cecc
46slv <46silverd@gmail.com>
2026-04-05 02:54:05 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,7 @@
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
-
-export default defineConfig({
-  site: 'https://46silverd.com',
-  // baseの行は書かない
-  vite: {
-    plugins: [tailwindcss()],
-  },
-=======
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
->>>>>>> 9f1dbef (fix: remove base path)
   adapter: cloudflare(),
 });
