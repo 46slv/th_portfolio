7a8412f761ce3d644a95e92bddc30b9970004e34
46slv <46silverd@gmail.com>
2026-04-05 01:53:59 +0900

    Add: Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://[ユーザー名].github.io/[リポジトリ名] の形式にします
 export default defineConfig({
   site: 'https://46slv.github.io',
-  base: '/th_portfolio', // リポジトリ名をここに入れます
+
+  // リポジトリ名をここに入れます
+  base: '/th_portfolio',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
