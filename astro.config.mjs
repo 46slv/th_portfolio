c5eaaf38fb9f93ed6a9596ab5792de2a6daf9dff
46slv <46silverd@gmail.com>
2026-04-05 01:48:15 +0900

    Deploy: Update config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+// https://[ユーザー名].github.io/[リポジトリ名] の形式にします
+export default defineConfig({
+  site: 'https://46slv.github.io',
+  base: '/th_portfolio', // リポジトリ名をここに入れます
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
